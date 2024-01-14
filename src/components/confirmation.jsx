65e568415b5f88ec70163c44c3496fb7e4cd20b8
Gian Montanet <gianmontanet@hotmail.com>
2024-01-14 20:17:07 -0300

    Componente Confirmation (confirmação de reserva)

---
+++ src/components/confirmation.jsx
@@ -0,0 +1,37 @@
+import '../styles/confirmation.css'
+import Button from './button';
+
+const Confirmation = ({ name, phone, availableSeats}) => {
+    const options = Array.from({ length: availableSeats }, (_, index) => index + 1);
+    return (
+        <div>
+            <h1>2. Preencha o cadastro</h1>
+            <form className="info">
+                <label>Nome: {name}</label>
+
+                <label className="inline-label">Telefone: {phone}</label><br />
+
+                <label>Número de lugares:
+                    <select>
+                        <option value="">Selecione</option>
+                        {options.map((number) => (
+                            <option key={number} value={number}>
+                                {number}
+                            </option>
+                        ))}
+                    </select>
+                </label><br />
+
+                <label>Pedido especial:<br />
+                    <textarea maxLength={1000} />
+                </label>
+
+
+                <Button label="Reservar!" />
+            </form>
+
+        </div>
+    );
+}
+
+export default Confirmation;
