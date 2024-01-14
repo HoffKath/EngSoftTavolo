215b40d5f383c93dc2a7fd969cff2f05a2256a09
bigparty2 <kauesgomes22@gmail.com>
2024-01-14 11:53:03 -0300

    Componente botão

---
+++ src/components/button.jsx
@@ -0,0 +1,12 @@
+import '../styles/button.css'
+
+function Button({label, onClick})
+{
+    return (
+        <button className="button" onClick={onClick}>
+            {label}
+        </button>
+    );
+}
+
+export default Button;
