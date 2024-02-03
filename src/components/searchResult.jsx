740b9e4b8a140b9efcdc67b8cc2d26e07f9b3e6d
Kauê Gomes <kauesgomes22@gmail.com>
2024-02-03 18:41:46 -0300

    utilização da classe restaurant para modulo de resultado de pesquisa

--- src/components/searchResult.jsx
+++ src/components/searchResult.jsx
@@ -1,12 +1,11 @@
 import '../styles/searchResult.css';
 import RestaurantPreview from './restaurantPreview';
 
-
 const SearchResult = ({ restaurants }) => {
   return (
     <div className="restaurants-container">
       {restaurants.map((restaurant) => (
-        <RestaurantPreview restaurantImg={} restaurantName={} restaurantRating={}/>
+        <RestaurantPreview restaurantImg={restaurant.logo} restaurantName={restaurant.restaurantName} restaurantRating={restaurant.rating}/>
       ))}
     </div>
   );
