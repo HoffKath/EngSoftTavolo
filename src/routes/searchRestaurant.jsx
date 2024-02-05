6ea127ad025a3b328cc0d9ea4ce6da70270993b8
Kauê Gomes <kauesgomes22@gmail.com>
2024-02-05 17:34:17 -0300

    Pagina pesquisa

--- src/routes/searchRestaurant.jsx
+++ src/routes/searchRestaurant.jsx
@@ -1,8 +1,13 @@
 import SearchInput from "../components/searchInput"
+import SearchResult from "../components/searchResult";
+import Restaurant from "../data/restaurant"
 
 import React, { useState } from 'react';
 
 const SearchRestaurant = () =>{
+
+  let restaurants;
+  // let restaurants = [new Restaurant(0, 0, 0, 0, 0, 0, 0, 0, 3.7, "restaurantName")]; //test
 
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -10,16 +15,15 @@ const SearchRestaurant = () =>{
     setSearchTerm(value);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // ... //
-    props.onSearch(searchTerm);
+  const handleSubmit = (value) => {
+    // lógica captura restaurants  //
   };
 
   return (
-    <div className="login">
+    <div className="search-page">
         <p>
-        <SearchInput placeholder="Escreva o nome do restaurante aqui" value={searchTerm} onClick={handleSubmit} onChange={handleInputChange}/>
+          <SearchInput placeholder="Escreva o nome do restaurante aqui" value={searchTerm} onClick={handleSubmit} onChange={handleInputChange}/>
+          <SearchResult restaurants={restaurants}/>
         </p>
     </div>
   );
