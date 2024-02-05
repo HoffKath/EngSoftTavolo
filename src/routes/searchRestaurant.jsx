00083eea3b8dfe879fae05b9f046531472008bd6
Kauê Gomes <kauesgomes22@gmail.com>
2024-02-05 14:51:36 -0300

    Callback SearchInput

--- src/routes/searchRestaurant.jsx
+++ src/routes/searchRestaurant.jsx
@@ -1,15 +1,29 @@
 import SearchInput from "../components/searchInput"
+
+import React, { useState } from 'react';
+
 const SearchRestaurant = () =>{
 
-    return (
-      <div className="login">
-          <p>
-          <SearchInput/>
-          </p>
-      </div>
-    );
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (value) => {
+    setSearchTerm(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // ... //
+    props.onSearch(searchTerm);
+  };
+
+  return (
+    <div className="login">
+        <p>
+        <SearchInput placeholder="Escreva o nome do restaurante aqui" value={searchTerm} onClick={handleSubmit} onChange={handleInputChange}/>
+        </p>
+    </div>
+  );
   
 }
 
-  
 export default SearchRestaurant;
