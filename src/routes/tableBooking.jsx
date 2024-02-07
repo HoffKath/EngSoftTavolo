d2ec682f9d5e95d9e1561ec43e8bc09979220431
Kauê Gomes <kauesgomes22@gmail.com>
2024-02-06 22:57:08 -0300

    adaptação da TableBooking para recepção de parametro e captura de restaurante

--- src/routes/tableBooking.jsx
+++ src/routes/tableBooking.jsx
@@ -1,13 +1,32 @@
 import ChooseHour from "../components/chooseHour";
 import Confirmation from "../components/confirmation";
 import RestaurantBanner from "../components/restaurantBanner";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 //import '../styles/tableBooking.css'
 import '../styles/confirmation.css'
+import Restaurant from "../data/restaurant";
+import RestaurantService from '../services/csv/restaurantService';
 
-const TableBooking =()=>{
+const TableBooking = () =>{
 
   const [selectedItem, setItemChange] = useState(null);
+  const [restaurant, setRestaurant] = useState(new Restaurant());
+
+  const { restaurantId } = useParams();
+
+  useEffect(() => {
+    const fetchRestaurant = async () => {
+      try {
+        const restaurantData = await RestaurantService().getRestaurantById(restaurantId);
+        setRestaurant(restaurantData);
+      } catch (error) {
+        console.error('Error fetching restaurant:', error);
+      }
+    };
+
+    fetchRestaurant();
+  }, [restaurantId]);
 
   const handleChange = (newItem) => {
       setItemChange(newItem);
@@ -16,7 +35,7 @@ const TableBooking =()=>{
 
   return (
     <div>
-        <RestaurantBanner/>
+        <RestaurantBanner name = {restaurant.name} rating = {restaurant.rating} phone={restaurant.cellphone} adress={restaurant.address} VegOption={restaurant.isVegan} GlutenFreeOption={restaurant.isGlutenFree} picture={restaurant.logo}/>
         <ChooseHour onChangeSelected={handleChange}/>
         <Confirmation availableSeats={selectedItem}/>
     </div>
